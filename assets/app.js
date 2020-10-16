ad954eb449ecd8db14cf26f7d84389e0420285ea
n-deleforge <contact@nicolas-deleforge.fr>
2020-10-16 10:09:51 +0200

    Cleaning the app and add comments in JS

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,6 @@
 // =================================================
-// ==================================== INITIALISATION
 // =================================================
+// ============ INITIALISATION
 
 const version = 1.3; // the actual version of the app
 const linkGitHub = "<a target=\"_blank\" href=\"https://github.com/n-deleforge/class-manager-generator\">GitHub</a>";
@@ -58,26 +58,29 @@ let values = Object.values(display);
 for (let i = 0; i < idName.length; i++) get("#" + idName[i]).innerHTML = values[i];
 
 // =================================================
-// ============================================ MAIN
 // =================================================
+// ============ MAIN
 
-// =================> Manage the generate button
-// ========================================
+// ===> Manage the generate button
 get("#generate").addEventListener("click", function () {
-    let error = 0;
-    let list = get(".gen");
+    let error = 0; // error count
+    let list = get(".gen"); // list of all inputs
 
     for (let i = 0; i < list.length; i++) {
-        if (list[i].value === "") error++;
+        // increment error if an input is empty
+        if (list[i].value === "") error++; 
     }
 
     if (error == 0) {
         get("#error").style.display = "none";
+
+        // Fulfill variables with correct data
         tableName = get("#tableName").value;
         tableId = get("#tableId").value;
         className = ucFirst(get("#className").value);
         columnsName = get("#columnsName").value.split(',');
 
+        // Generate data and make it downloadable
         download(generateClass(), className + ".Class.php");
         download(generateManager(), className + "Manager.Class.php");
     }
@@ -85,12 +88,15 @@ get("#generate").addEventListener("click", function () {
 });
 
 // =================================================
-// =========================================== CLASS
+// =================================================
+// ============ CLASS
 
+// ===> Merge together all the class functions
 function generateClass() {
     return "<?php" + "\n" + 'class ' + className + "\n{\n" + generateAttributes() + "\n" + generateGetterSetter() + "\n" + genererConstruct() + "\n\n}";
 }
 
+// ===> Create the attributes part
 function generateAttributes() {
     let attributes = "";
 
@@ -101,6 +107,7 @@ function generateAttributes() {
     return attributes;
 }
 
+// ===> Create the getter/setter part
 function generateGetterSetter() {
     let getterSetter = "";
 
@@ -112,6 +119,7 @@ function generateGetterSetter() {
     return getterSetter;
 }
 
+// ===> Create the construct part
 function genererConstruct() {
     return `public function __construct(array $options = [])
     {
@@ -134,8 +142,10 @@ function genererConstruct() {
 }
 
 // =================================================
-// ======================================== MANAGER
+// =================================================
+// ============ MANAGER
 
+// ===> Merge together all the manager functions
 function generateManager() {
     let key = columnsName.find(element => element == tableId);
     if (key != "undefined") columnsName.splice(columnsName.indexOf(key), 1)
@@ -143,6 +153,7 @@ function generateManager() {
     return "<?php\nclass " + className + "Manager\n{\n" + generateAdd() + "\n\n" + generateUpdate() + "\n\n" + generateDelete() + "\n\n" + generateFindById() + "\n\n" + generateGetList() + "\n\n}";
 }
 
+// ===> Create the add functiion
 function generateAdd() {
     let attributesList = "";
     let sqlValues = "";
@@ -160,6 +171,7 @@ function generateAdd() {
     return "public static function add(" + className + " $obj)\n{\n$db = DbConnect::getDb();\n" + '$q = $db->prepare("INSERT INTO ' + tableName + ' (' + attributesList + ') VALUES (' + sqlValues + ')");\n' + bindsList + "$q->execute();\n}";;
 }
 
+// ===> Create the update function
 function generateUpdate() {
     let attributesList = "";
     let bindsList = "";
@@ -175,40 +187,40 @@ function generateUpdate() {
     return 'public static function update(' + className + " $obj)\n{\n$db = DbConnect::getDb();\n" + '$q = $db->prepare("UPDATE ' + tableName + ' SET ' + attributesList + " WHERE " + tableId + "=:" + tableId + "\");\n" + bindsList + "$q->execute();\n}";
 }
 
+// ===> Create the delete function
 function generateDelete() {
     return 'public static function delete(' + className + " $obj)\n{\n$db = DbConnect::getDb();\n" + '$db->exec("DELETE FROM ' + tableName + ' WHERE ' + tableId + '=" . $obj->get' + ucFirst(tableId) + '());\n}';
 }
 
+// ===> Create the findById function
 function generateFindById() {
     return "public static function findById($id)\n{\n$db = DbConnect::getDb();\n$id = (int) $id;\n" + '$q = $db->query("SELECT * FROM ' + tableName + ' WHERE ' + tableId + '=".$id);\n' + "$results = $q->fetch(PDO::FETCH_ASSOC);\nif ($results != false) {\nreturn new " + className + " ($results);\n }else {\nreturn false;\n}\n}";
 }
 
+// ===> Create the getList function
 function generateGetList() {
     return "public static function getList()\n{\n$db = DbConnect::getDb();\n$langage = [];\n" + '$q = $db->query("SELECT * FROM ' + tableName + '");\n' + "while ($donnees = $q->fetch(PDO::FETCH_ASSOC)) {\nif ($donnees != false) {\n$langage[] = new " + className + "($donnees);\n}\n}\nreturn $langage;\n}";
 }
 
 // =================================================
-// ========================================= GENERIC
 // =================================================
+// ============ GENERIC
 
-// =========================> Select an element
-// ========================================
+// ===> Select an element
 function get(n) {
     if (n.search("#") != -1 && document.getElementById(n.split("#")[1]) != null) { return document.getElementById(n.split("#")[1]); }
     if (n.search("~") != -1 && document.querySelectorAll(n.split('~')[1]) != null) { return document.querySelectorAll(n.split('~')[1]); }
     if (n.search("\\.") != -1 && document.querySelectorAll(n).length != 0) { return document.querySelectorAll(n); }
 }
 
-// ================> Faster usage of localStorage
-// ========================================
+// ===> Faster usage of localStorage
 function storage(a, n, v) {
     if (a == "get") return localStorage.getItem(n);
     if (a == "set") return localStorage.setItem(n, v);
     if (a == "rem") return localStorage.removeItem(n);
 }
 
-// ==========================> Download a file
-// ========================================
+// ===> Download a file
 function download(c, n) {
     let file = new Blob([c], {
         type: 'text/plain'
@@ -220,8 +232,7 @@ function download(c, n) {
     dl.click();
 }
 
-// ==========================> Add a majuscule
-// ========================================
+// ===> Add a majuscule
 function ucFirst(s) {
     return s.charAt(0).toUpperCase() + s.slice(1);
 }
