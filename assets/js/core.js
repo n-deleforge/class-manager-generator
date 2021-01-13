e60664c5302de221390c86faf6d0d8ef72b3f271
n-deleforge <contact@nicolas-deleforge.fr>
2021-01-13 10:28:51 +0100

    Cleaning JS core file

--- assets/js/core.js
+++ assets/js/core.js
@@ -2,20 +2,14 @@
 // =================================================
 // ============ CORE VARIABLES
 
-const version = 1.5;
-const githubLink = "<a target=\"_blank\" href=\"https://github.com/n-deleforge/class-manager-generator\">GitHub</a>";
-const homeLink = "<a target=\"_blank\" href=\"https://nicolas-deleforge.fr/\">nd</a>";
-let tableName; let tableId; let className; let columnsName;
-
-// =================================================
-// =================================================
-// ============ CORE INITIALISATION
-
-// ===> French translation
-const french = {
+let tableName; let tableId; let columnsName; let className; 
+const VERSION = 1.5;
+const GITHUB = "<a target=\"_blank\" href=\"https://github.com/n-deleforge/class-manager-generator\">GitHub</a>";
+const HOME = "<a target=\"_blank\" href=\"https://nicolas-deleforge.fr/\">nd</a>";
+const FRENCH = {
     'generate' : "Générer",
     'error' : "Tous les champs sont nécessaires.",
-    'footer' : "Disponible sur " + githubLink + " (v " + version + ") - Hébergé sur " + homeLink,
+    'footer' : "Disponible sur " + GITHUB + " (v " + VERSION + ") - Hébergé sur " + HOME,
     'tableNameLabel' : "➡ NOM DE LA TABLE",
     'tableNameDesc' : "Nom de table qui correspond au nom tel qu'il est dans votre base de données SQL.",
     'tableIdLabel' : "➡ NOM DU CHAMP ID",
@@ -24,13 +18,11 @@ const french = {
     'columnsNameDesc' : "Les différentes colonnes de votre table SQL (doit respecter le format exact suivant : \"col1,col2,col3\").",
     'classNameLabel' : "➡ NOM CLASS/MANAGER",
     'classNameDesc' : "Nom de la Class et du Manager que vous allez créer (une majuscule est automatiquement appliquée)."
-}
-
-// ===> English translation
-const english = {
+};
+const ENGLISH = {
     'generate' : "Generate",
     'error' : "All the fields have to be filled.",
-    'footer' : "Available on " + githubLink + " (v " + version + ") - Hosted on " + homeLink,
+    'footer' : "Available on " + GITHUB + " (v " + VERSION + ") - Hosted on " + HOME,
     'tableNameLabel' : "➡ TABLE NAME",
     'tableNameDesc' : "Name of the table in your database.",
     'tableIdLabel' : "➡ ID FIELD NAME",
@@ -39,21 +31,16 @@ const english = {
     'columnsNameDesc' : "All the columns' names of your table. Must respect this format : col1,col2,col3",
     'classNameLabel' : "➡ CLASS/MANAGER NAME",
     'classNameDesc' : "Name of the Class and the Manager."
-}
+};
 
-// ===> Determine the language of the app
-if (navigator.language == "fr" || navigator.language == "fr-FR") {
-    display = french;
-    get("#htmlTag").lang = "fr";
-}
-else {
-    display = english;
-    get("#htmlTag").lang = "en";
-}
+// =================================================
+// =================================================
+// ============ CORE INITIALISATION
 
-// ===> Automatically fill all ID fields
-let names = Object.keys(display);
-let values = Object.values(display);
+// ===> Determine the language of the application
+const CONTENT = (navigator.language == "fr" || navigator.language == "fr-FR") ? FRENCH : ENGLISH;
+let names = Object.keys(CONTENT);
+let values = Object.values(CONTENT);
 
 for (let i = 0; i < names.length; i++) {
     if (get("#" + names[i])) {
